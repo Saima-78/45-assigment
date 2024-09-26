6bacc5a449966338387b5effacace2c3791862f6
Saima-78 <Saimaanwer681@gmail.com>
2024-09-26 11:15:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict";
+//question2:
+//let personName="saima";
+//console.log('hello',personName,"would you like to learn some typescript")
+//question3:
+//let personName="Saima"
+//console.log("Saima\nSAIMA\nsaima");
+//question4
+//let personName="Saima";
+//console.log('"Quaid e Azam said","work work or work"'):
+//Question5
+//let famousPerson="Quaid e Azam";
+// message="said,'work work and work we are bound succeed'";
+//console.log(famousPerson,message)
+//Question6
+//let whitespace="\n\t Hamza Alvi\t\n";
+//console.log(whitespace);
+//let withoutwhitespace=whitespace.trim(); 
+//console.log(withoutwhitespace);
+//Question7
+//console.log(6+2);
+//console.log(10-2);   
+//console.log(2*4);
+//console.log(24/3)
+//Question8
+//console.log(4+4);
+//console.log(12-4);
+//console.log(8*1);
+//console.log(16/2);
+//Question9
+//let favouriteNumber=14
+//console.log("My favourite number is" , favouriteNumber);
+//Question10
+Object.defineProperty(exports, "__esModule", { value: true });
---
+++ main.ts
@@ -0,0 +1,33 @@
+//question2:
+//let personName="saima";
+//console.log('hello',personName,"would you like to learn some typescript")
+//question3:
+//let personName="Saima"
+//console.log("Saima\nSAIMA\nsaima");
+//question4
+//let personName="Saima";
+//console.log('"Quaid e Azam said","work work or work"'):
+//Question5
+//let famousPerson="Quaid E Azam";
+//let message="said,'work work and work and never get tired of working'";
+//console.log(famousPerson,message);
+//Question6
+//let whitespace="\n\t Hamza Alvi\t\n";
+//console.log(whitespace);
+//let withoutwhitespace=whitespace.trim(); 
+//console.log(withoutwhitespace);
+//Question7
+//console.log(6+2);
+//console.log(10-2);   
+//console.log(2*4);
+//console.log(24/3)
+//Question8
+//console.log(4+4);
+//console.log(12-4);
+//console.log(8*1);
+//console.log(16/2);
+//Question9
+//let favouriteNumber=14
+//console.log("My favourite number is" , favouriteNumber);
+//Question10
+
